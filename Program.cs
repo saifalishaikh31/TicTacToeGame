604f08c52981aa939c5a937b46a799d93a3e515b
saifalishaikh31 <saifalishaikh31>
2022-02-10 10:10:53 +0530

    [Saif Ali] Add: Intitialize Board

---
+++ Program.cs
@@ -0,0 +1,8 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        TicTacToeGame game = new TicTacToeGame();
+        game.Game();
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,12 @@
+class TicTacToeGame
+{
+    char[] board = new char[10];
+    public void Game()
+    {
+        for(int i=1; i < 9; i++)
+        {
+            board[i] = ' ';            
+        }
+    }
+}
+
